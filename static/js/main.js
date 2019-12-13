ca8a9928ca010a436a003f6c95f47c53251273ce
grzegorz.mika <gmika@interia.pl>
2019-12-13 22:18:56 +0100

    landing page first commit

---
+++ static/js/main.js
@@ -0,0 +1,24 @@
+
+
+const slider= document.querySelector("#slider");
+const sliderText= document.querySelector("#sliderText");
+
+const slides = slider.getElementsByTagName('img');
+const slidesText = sliderText.getElementsByTagName('div');
+
+let index = 0;
+
+function nextSlide(forward_backward){
+    slides[index].classList.remove('active');
+    slidesText[index].classList.remove('active');
+    index = (index+forward_backward) % slides.length;
+    if (index < 0){
+        index = slides.length-1;
+    }
+    slides[index].classList.add('active');
+    slidesText[index].classList.add('active');
+}
+
+
+
+
