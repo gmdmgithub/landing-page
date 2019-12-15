41a57d27eb0c91370f1cc6ec11ec48b30f4ebf3a
grzegorz.mika <gmika@interia.pl>
2019-12-15 19:11:08 +0100

    project file added

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,16 @@
 
 
 const slider= document.querySelector("#slider");
-const sliderText= document.querySelector("#sliderText");
+let sliderText= null;
+let slides = null;
+let slidesText = null;
 
-const slides = slider.getElementsByTagName('img');
-const slidesText = sliderText.getElementsByTagName('div');
+if( slider) {
+    sliderText = document.querySelector("#sliderText");
+    slides = slider.getElementsByTagName('img');
+    slidesText = sliderText.getElementsByTagName('div');
 
+}
 let index = 0;
 
 function nextSlide(forward_backward){
@@ -25,6 +30,38 @@ function menuToggle(){
     nav.classList.toggle('active');
     
 }
+
+const project_tab = document.querySelector('.content_products')
+
+if (project_tab){
+
+    function elapsedTime(){
+        const countDownDate = new Date('2020-03-01 12:00:00').getTime();
+        const now = new Date().getTime();
+        const gap = countDownDate - now;
+
+        const second = 1000;
+        const minute = 60*second;
+        const hour = 60*minute;
+        const day = 24*hour;
+
+        const d_dif = Math.floor(gap/day);
+        const h_dif = Math.floor((gap%day)/hour);
+        const m_dif = Math.floor((gap%hour)/minute);
+        const s_dif = Math.floor((gap%minute)/second);
+
+        document.querySelector('#day').innerHTML = d_dif;
+        document.querySelector('#hour').innerHTML = h_dif;
+        document.querySelector('#minute').innerHTML = m_dif;
+        document.querySelector('#second').innerHTML = s_dif;
+    }
+
+    setInterval(function(){
+        elapsedTime()
+    },1000);
+
+}
+
 
 
 
